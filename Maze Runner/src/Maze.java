0759706dd4ff42c6bdfd9981dba02aa9d2370299
dfriedman <dfriedman@10.0.13.175>
2023-03-21 09:53:14 -0700

    maze generation made more efficient

--- Maze Runner/src/Maze.java
+++ Maze Runner/src/Maze.java
@@ -12,83 +12,43 @@ import javax.swing.JPanel;
 public class Maze {
 	
 
-	private int WWIDTH = 700, WHEIGHT = 700;
-	public final int ROWS = 25, COLS = 25;
-	public int BOXW = WWIDTH / ROWS, BOXH = WHEIGHT / COLS;
+	public final int ROWS, COLS;
 
 	// true = white, false = black
-	private boolean[][] maze;
+	public boolean[][] grid;
 	
 	private final int ANIMATESPEED = 10;
 	
-	private final double PCTFILLED = .8;
+	private final double PCTFILLED;
 	
 	private ArrayList<Point> mainPathPoints;
 	
+	public Point start;
+	
 	private final double[] DIRECTIONWEIGHTS = {4, 3, 1}; // right, up/down, left
 	
-	private JFrame frame;
 	
-	public Maze(boolean animate) {
-		if (animate) {
-			frame = new JFrame();
-			frame.setSize(WWIDTH, WHEIGHT);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-			JPanel canvas = new JPanel() {
-				public void paintComponent(Graphics g) {
-					g.setColor(Color.white);
-					g.fillRect(0, 0, WWIDTH, WHEIGHT);
-					g.setColor(Color.BLACK);
-					int extraPixelsW = WWIDTH - BOXW * COLS;
-					int extraPixelsH = WHEIGHT - BOXH * ROWS;
-					for (int i = 0; i < maze.length; i++) {
-						int shiftH = i < extraPixelsH ? 1 : 0;
-						for (int j = 0; j < maze[i].length; j++) {
-							int shiftW = j < extraPixelsW ? 1 : 0;
-							if (!maze[i][j])
-								g.fillRect(j * BOXW + Math.min(j, extraPixelsW), i * BOXH + Math.min(i, extraPixelsH),
-										BOXW + shiftW, BOXH + shiftH);
-							else
-								g.drawRect(j * BOXW + Math.min(j, extraPixelsW), i * BOXH + Math.min(i, extraPixelsH),
-										BOXW + shiftW, BOXH + shiftH);
-
-						}
-					}
-				}
-			};
-			
-			// resizes the maze as the user drags edges
-			canvas.addComponentListener(new ComponentAdapter() {
-				public void componentResized(ComponentEvent evt) {
-					WWIDTH = canvas.getWidth();
-					WHEIGHT = canvas.getHeight();
-					BOXW = WWIDTH / ROWS;
-					BOXH = WHEIGHT / COLS;
-				}
-			});
-
-			frame.add(canvas);
-			frame.setLocationRelativeTo(null);
-			frame.setVisible(true);
-			
-			frame.getContentPane().repaint();
-		}
-		while (!fillMaze(animate));
+	public Maze(int rows, int cols, double pctFilled) {
+		ROWS = rows;
+		COLS = cols;
+		PCTFILLED = pctFilled;
+		grid = new boolean[ROWS][COLS];
 	}
 	
 	
-	public boolean fillMaze(boolean animate) {
-		maze = new boolean[ROWS][COLS];
+	public void fillMaze(boolean animate, JFrame frame) {
+		grid = new boolean[ROWS][COLS];
 		mainPathPoints = new ArrayList<Point>();
 		Point cp = new Point(0, (int)(Math.random() * ROWS));
-		maze[cp.y][cp.x] = true;
+		start = cp;
+		grid[cp.y][cp.x] = true;
 		
 		while (cp.x < COLS - 2) {
 			if ((cp = randPoint(cp))== null) {
-				return false;
+				fillMaze(animate, frame);
+				return;
 			}
-			maze[cp.y][cp.x] = true;
+			grid[cp.y][cp.x] = true;
 			if (animate) {
 				try {
 					Thread.sleep(ANIMATESPEED);
@@ -100,15 +60,12 @@ public class Maze {
 			}
 			mainPathPoints.add(cp);
 		}
-		maze[cp.y][cp.x+1] = true;
+		grid[cp.y][cp.x+1] = true;
 		
 		if (animate)
 			frame.getContentPane().repaint();
 		
-		while (pctFilled() < PCTFILLED && buildBranch(animate));
-		
-		return true;
-		
+		while (pctFilled() < PCTFILLED && buildBranch(animate, frame));		
 	}
 	
 	// returns a random neighbor that isn't already white and 
@@ -118,18 +75,18 @@ public class Maze {
 		for (int i = 0; i < nei.size(); i++) {
 			Point p = nei.get(i);
 			if (p.x == COLS-2) return p;
-			if (maze[p.y][p.x]) {
+			if (grid[p.y][p.x]) {
 				nei.remove(i);
 				i--;
 			}
 			
-			else if (p.y < maze.length-1&& p.y+1 != cp.y && maze[p.y + 1][p.x]) {
+			else if (p.y < grid.length-1&& p.y+1 != cp.y && grid[p.y + 1][p.x]) {
 				nei.remove(i); i--;}
-			else if (p.x < maze[0].length-1 && p.x+1 != cp.x && maze[p.y][p.x+1]) {
+			else if (p.x < grid[0].length-1 && p.x+1 != cp.x && grid[p.y][p.x+1]) {
 				nei.remove(i);i--;}
-			else if (p.y > 0 && p.y-1 != cp.y && maze[p.y - 1][p.x]) {
+			else if (p.y > 0 && p.y-1 != cp.y && grid[p.y - 1][p.x]) {
 				nei.remove(i);i--;}
-			else if (p.x > 0 && p.x-1 != cp.x && maze[p.y][p.x-1]) {
+			else if (p.x > 0 && p.x-1 != cp.x && grid[p.y][p.x-1]) {
 				nei.remove(i);i--;}
 		}
 		
@@ -166,25 +123,25 @@ public class Maze {
 	// returns the legal neighbors of point p
 	private ArrayList<Point> neighbors(Point p, boolean filled) {
 		ArrayList<Point> neighbors = new ArrayList<Point>();
-		if (p.y > 1 && maze[p.y - 1][p.x] == filled)
+		if (p.y > 1 && grid[p.y - 1][p.x] == filled)
 			neighbors.add(new Point(p.x, p.y - 1));
-		if (p.y < maze.length-2 && maze[p.y + 1][p.x] == filled)
+		if (p.y < grid.length-2 && grid[p.y + 1][p.x] == filled)
 			neighbors.add(new Point(p.x, p.y + 1));
-		if (p.x > 1 && maze[p.y][p.x - 1] == filled)
+		if (p.x > 1 && grid[p.y][p.x - 1] == filled)
 			neighbors.add(new Point(p.x - 1, p.y));
-		if (p.x < maze[0].length-2 && maze[p.y][p.x + 1]== filled)
+		if (p.x < grid[0].length-2 && grid[p.y][p.x + 1]== filled)
 			neighbors.add(new Point(p.x + 1, p.y));
 		return neighbors;
 	}
-	// adds a side branch to the maze
-	private boolean buildBranch(boolean animate) {
+	// adds a side branch to the grid
+	private boolean buildBranch(boolean animate, JFrame frame) {
 		Point lastP = null;
 		Point lastDir = null;
-		int branchLength = (int)(Math.random()*maze.length+maze.length/4);
+		int branchLength = (int)(Math.random()*grid.length+grid.length/4);
 		if (mainPathPoints.size() <= 0) 
 			return false;
 		Point cp = mainPathPoints.remove((int)(Math.random()*mainPathPoints.size()));
-		for (int i = 0; i < branchLength && cp.y < maze.length-1; i++) {
+		for (int i = 0; i < branchLength && cp.y < grid.length-1; i++) {
 			if (animate) {
 				try {
 					Thread.sleep(75);
@@ -196,12 +153,12 @@ public class Maze {
 			}
 			cp = randPoint(cp);
 			if (cp == null) {
-				if (lastDir!= null && lastP.x+lastDir.x < maze[0].length-1 && lastP.x+lastDir.x > 0
-						&& lastP.y+lastDir.y < maze.length && lastP.y+lastDir.y > 0)
-					maze[lastP.y+lastDir.y][lastP.x+lastDir.x] = true;
+				if (lastDir!= null && lastP.x+lastDir.x < grid[0].length-1 && lastP.x+lastDir.x > 0
+						&& lastP.y+lastDir.y < grid.length && lastP.y+lastDir.y > 0)
+					grid[lastP.y+lastDir.y][lastP.x+lastDir.x] = true;
 				return true;
 			}
-			maze[cp.y][cp.x] = true;
+			grid[cp.y][cp.x] = true;
 			if (lastP!= null) lastDir = new Point(cp.x-lastP.x,cp.y-lastP.y);
 			lastP = cp;
 		}
@@ -210,14 +167,11 @@ public class Maze {
 	
 	public double pctFilled() {
 		double numFilled = 0;
-		for (boolean[] row : maze) 
+		for (boolean[] row : grid) 
 			for (boolean b : row) 
 				if (b) numFilled++;
 					
-		return numFilled/(maze.length*maze[0].length);
+		return numFilled/(grid.length*grid[0].length);
 	}
 
-public static void main(String[] args) {
-	new Maze(true);
-}
 }
--- Maze Runner/src/MazeRunner.java
+++ Maze Runner/src/MazeRunner.java
@@ -9,9 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import javax.swing.AbstractAction;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,7 +17,10 @@ import javax.swing.KeyStroke;
 
 public class MazeRunner {
 	
-	private int speed = 1; // default is 100- smaller = slower
+	private int speed = 100; // default is 100- smaller = slower
+	
+	private boolean animate = false;	// whether you want to animate maze creation
+
 	
 	// constructs and adds the bots competing into the maze
 	private void addBots() {
@@ -29,45 +30,30 @@ public class MazeRunner {
 			robots.put(b, new RobotInfo());
 	}
 	
-	
+	// ****************************************************************//
 	// feel free to peruse the below code, but you don't need to touch it.
 	
 	private int WWIDTH = 700, WHEIGHT = 700;
 	public final int ROWS = 25, COLS = 25;
 	private int BOXW = WWIDTH / ROWS, BOXH = WHEIGHT / COLS;
-	private final int diff = 3; // scale starting from 1
 	private int count = 0;
 	private boolean paused = false;
+	private double PCTFILLED = .8;
 
 	// true = white, false = black
-	private boolean[][] maze = new boolean[ROWS][COLS];
+	private Maze maze;
 	
 	private HashMap<Bot, RobotInfo> robots = new HashMap<Bot, RobotInfo>();
-
-	private Point begin;
-	private Point goal;
-
-	private Point p1, p2;
 	
-	private double pctFilled = 0;
 	private Bot lastbot = null;
 	
 	private JFrame frame;
 	
-	private boolean animate = false;
-	
-	private ArrayList<Point> mainPathPoints;
-
-	private final int ANIMATESPEED = 10;
-	
-	private final double PCTFILLED = .8;
-	
-	private final double[] DIRECTIONWEIGHTS = {4, 3, 1}; // right, up/down, left
 
 		
 	// class to keep track of robots' locations
 	private class RobotInfo {
-		private Point loc = new Point(begin.x,begin.y), dir = new Point(1,0);
+		private Point loc = new Point(maze.start.x, maze.start.y), dir = new Point(1,0);
 		public String toString() { return loc.x + ", " + loc.y;}
 	}
 	
@@ -84,7 +70,7 @@ public class MazeRunner {
 		// tries to move the bot in its current direction
 		RobotInfo r = robots.get(b);
 		if (r.loc.x+r.dir.x < 0) return false;
-		if (maze[r.loc.y+r.dir.y][r.loc.x+r.dir.x]) {
+		if (maze.grid[r.loc.y+r.dir.y][r.loc.x+r.dir.x]) {
 			r.loc.x += r.dir.x; r.loc.y+=r.dir.y;
 			return true;
 		}
@@ -110,149 +96,10 @@ public class MazeRunner {
 		else r.dir = new Point(1,0);
 	}
 
-	
-	public boolean fillMaze(boolean animate) {
-		maze = new boolean[ROWS][COLS];
-		mainPathPoints = new ArrayList<Point>();
-		Point cp = new Point(0, (int)(Math.random() * ROWS));
-		maze[cp.y][cp.x] = true;
-		
-		while (cp.x < COLS - 2) {
-			if ((cp = randPoint(cp))== null) {
-				return false;
-			}
-			maze[cp.y][cp.x] = true;
-			if (animate) {
-				try {
-					Thread.sleep(ANIMATESPEED);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				frame.getContentPane().repaint();
-			}
-			mainPathPoints.add(cp);
-		}
-		maze[cp.y][cp.x+1] = true;
-		begin = mainPathPoints.get(0);
-		goal = new Point(cp.x+1, cp.y);
-		
-		if (animate)
-			frame.getContentPane().repaint();
-		
-		while (pctFilled() < PCTFILLED && buildBranch(animate));
-		
-		return true;
-		
-	}
-	
-	// returns a random neighbor that isn't already white and 
-	// isn't adjacent to any white squares
-	private Point randPoint(Point cp) {
-		ArrayList<Point> nei = neighbors(cp, false);
-		for (int i = 0; i < nei.size(); i++) {
-			Point p = nei.get(i);
-			if (p.x == COLS-2) return p;
-			if (maze[p.y][p.x]) {
-				nei.remove(i);
-				i--;
-			}
-			
-			else if (p.y < maze.length-1&& p.y+1 != cp.y && maze[p.y + 1][p.x]) {
-				nei.remove(i); i--;}
-			else if (p.x < maze[0].length-1 && p.x+1 != cp.x && maze[p.y][p.x+1]) {
-				nei.remove(i);i--;}
-			else if (p.y > 0 && p.y-1 != cp.y && maze[p.y - 1][p.x]) {
-				nei.remove(i);i--;}
-			else if (p.x > 0 && p.x-1 != cp.x && maze[p.y][p.x-1]) {
-				nei.remove(i);i--;}
-		}
-		
-		if (nei.size() == 0) return null;
-		
-		ArrayList<Double> prob = new ArrayList<Double>();
-		double sum = 0;
-		for (int i = 0; i < nei.size(); i++) {
-			if (nei.get(i).x > cp.x)
-				prob.add(DIRECTIONWEIGHTS[0]);
-			else if (nei.get(i).x == cp.x)
-				prob.add(DIRECTIONWEIGHTS[1]);
-			else
-				prob.add(DIRECTIONWEIGHTS[2]);
-			
-			sum += prob.get(i);
-		}
-		
-		for (int i = 0; i < prob.size(); i++) {
-			prob.set(i, prob.get(i)/sum);
-		}
-		for (int i = 1; i < prob.size(); i++) {
-			prob.set(i, prob.get(i) + prob.get(i-1));
-		}
-		
-		double randprob = Math.random();
-		for (int i = 0; i < nei.size()-1; i++)
-			if (prob.get(i) > randprob)
-				return nei.get(i);
-		
-		return nei.get(nei.size()-1);
-	}
-	
-	// returns the legal neighbors of point p
-	private ArrayList<Point> neighbors(Point p, boolean filled) {
-		ArrayList<Point> neighbors = new ArrayList<Point>();
-		if (p.y > 1 && maze[p.y - 1][p.x] == filled)
-			neighbors.add(new Point(p.x, p.y - 1));
-		if (p.y < maze.length-2 && maze[p.y + 1][p.x] == filled)
-			neighbors.add(new Point(p.x, p.y + 1));
-		if (p.x > 1 && maze[p.y][p.x - 1] == filled)
-			neighbors.add(new Point(p.x - 1, p.y));
-		if (p.x < maze[0].length-2 && maze[p.y][p.x + 1]== filled)
-			neighbors.add(new Point(p.x + 1, p.y));
-		return neighbors;
-	}
-	// adds a side branch to the maze
-	private boolean buildBranch(boolean animate) {
-		Point lastP = null;
-		Point lastDir = null;
-		int branchLength = (int)(Math.random()*maze.length+maze.length/4);
-		if (mainPathPoints.size() <= 0) 
-			return false;
-		Point cp = mainPathPoints.remove((int)(Math.random()*mainPathPoints.size()));
-		for (int i = 0; i < branchLength && cp.y < maze.length-1; i++) {
-			if (animate) {
-				try {
-					Thread.sleep(75);
-				} catch (InterruptedException e) {
-					//  Auto-generated catch block
-					e.printStackTrace();
-				}
-				frame.getContentPane().repaint();
-			}
-			cp = randPoint(cp);
-			if (cp == null) {
-				if (lastDir!= null && lastP.x+lastDir.x < maze[0].length-1 && lastP.x+lastDir.x > 0
-						&& lastP.y+lastDir.y < maze.length && lastP.y+lastDir.y > 0)
-					maze[lastP.y+lastDir.y][lastP.x+lastDir.x] = true;
-				return true;
-			}
-			maze[cp.y][cp.x] = true;
-			if (lastP!= null) lastDir = new Point(cp.x-lastP.x,cp.y-lastP.y);
-			lastP = cp;
-		}
-		return true;
-	}
-	
-	public double pctFilled() {
-		double numFilled = 0;
-		for (boolean[] row : maze) 
-			for (boolean b : row) 
-				if (b) numFilled++;
-					
-		return numFilled/(maze.length*maze[0].length);
-	}
-
 	public MazeRunner() {
+		
+		maze = new Maze(ROWS, COLS, PCTFILLED);
+
 
 		frame = new JFrame();
 		frame.setSize(WWIDTH, WHEIGHT);
@@ -265,11 +112,11 @@ public class MazeRunner {
 				g.setColor(Color.BLACK);
 				int extraPixelsW = WWIDTH - BOXW * COLS;
 				int extraPixelsH = WHEIGHT - BOXH * ROWS;
-				for (int i = 0; i < maze.length; i++) {
+				for (int i = 0; i < maze.grid.length; i++) {
 					int shiftH = i < extraPixelsH ? 1 : 0;
-					for (int j = 0; j < maze[i].length; j++) {
+					for (int j = 0; j < maze.grid[i].length; j++) {
 						int shiftW = j < extraPixelsW ? 1 : 0;
-						if (!maze[i][j])
+						if (!maze.grid[i][j])
 							g.fillRect(j * BOXW + Math.min(j, extraPixelsW), i * BOXH + Math.min(i, extraPixelsH),
 									BOXW + shiftW, BOXH + shiftH);
 						else
@@ -278,10 +125,10 @@ public class MazeRunner {
 
 					}
 				}
-				g.setColor(Color.GREEN);
-				if (goal != null)
-					g.fillRect(goal.x * BOXW + Math.min(extraPixelsW, goal.x), goal.y * BOXH + Math.min(extraPixelsH, goal.y),
-						BOXW, BOXH);
+//				g.setColor(Color.GREEN);
+//				if (goal != null)
+//					g.fillRect(goal.x * BOXW + Math.min(extraPixelsW, goal.x), goal.y * BOXH + Math.min(extraPixelsH, goal.y),
+//						BOXW, BOXH);
 				for (Bot b : robots.keySet()) {
 					if (robots.get(b) == null) continue;
 					g.setColor(b.color);
@@ -315,7 +162,9 @@ public class MazeRunner {
 		canvas.getActionMap().put("Pause", new PauseAction());
 		
 		frame.getContentPane().repaint();
-		while(!fillMaze(animate));
+		
+		maze.fillMaze(animate, frame);
+		
 		addBots();
 		
 		// runs the maze until a robot has reached the end
