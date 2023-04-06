c540f4877b7aa733cb04c119f712649e90e65fda
dfriedman <dfriedman@10.0.13.175>
2023-04-06 13:10:46 -0700

    enums implemented

--- Pokemon/src/Effect.java
+++ Pokemon/src/Effect.java
@@ -5,7 +5,7 @@ public enum Effect {
 	NONE("", "", Color.white, 0), ASLEEP("ASP", "put to sleep", Color.gray, 0), POISONED("PSN", "poisoned", new Color(200,0,250), 7), 
 	CONFUSED("CNF", "confused", Color.red, 15), PARALYZED("PAR", "paralyzed", Color.orange, 0);
 	
-	public final String message, label;
+	public final String message, label; 
 	public final Color color;
 	public final int damage;
 	
